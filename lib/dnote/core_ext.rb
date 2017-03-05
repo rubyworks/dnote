b647e9ea602f32f1966dea3d4f999bb8b35c6995
Matijs van Zuijlen <matijs@matijs.net>
2017-03-05 12:07:48 +0100

    Autocorrect Style/RedundantParentheses

--- lib/dnote/core_ext.rb
+++ lib/dnote/core_ext.rb
@@ -29,8 +29,8 @@ module DNote
     def margin(n=0)
       #d = /\A.*\n\s*(.)/.match( self )[1]
       #d = /\A\s*(.)/.match( self)[1] unless d
-      d = ((/\A.*\n\s*(.)/.match(self)) ||
-          (/\A\s*(.)/.match(self)))[1]
+      d = (/\A.*\n\s*(.)/.match(self) ||
+          /\A\s*(.)/.match(self))[1]
       return '' unless d
       if n == 0
         gsub(/\n\s*\Z/, '').gsub(/^\s*[#{d}]/, '')
