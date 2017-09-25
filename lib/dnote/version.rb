09a1344ecc4ea4c80335cf0d9c905c855882f8d3
Matijs van Zuijlen <matijs@matijs.net>
2017-09-25 12:31:00 +0200

    Remove remaining bits of old metadata mechanism

--- lib/dnote/version.rb
+++ lib/dnote/version.rb
@@ -1,16 +1,3 @@
 module DNote
   VERSION = '1.7.2'.freeze
-
-  #
-  def self.metadata
-    @metadata ||= begin
-      require 'yaml'
-      YAML.load_file(File.dirname(__FILE__) + '/../dnote.yml')
-    end
-  end
-
-  #
-  def self.const_missing(name)
-    metadata[name.to_s.downcase] || super(name)
-  end
 end
