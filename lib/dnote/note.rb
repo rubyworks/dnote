6d5488ceee8fdad2445f4dc7eace6f230b099fb3
Matijs van Zuijlen <matijs@matijs.net>
2017-09-25 12:59:10 +0200

    Fix Style/NumericPredicate

--- lib/dnote/note.rb
+++ lib/dnote/note.rb
@@ -56,7 +56,7 @@ module DNote
     # Sort by file name and line number.
     def <=>(other)
       s = file <=> other.file
-      return s unless s == 0
+      return s unless s.zero?
       line <=> other.line
     end
 
