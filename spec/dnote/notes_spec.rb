7c1275e1f160cf454740919c7144227abe8d5681
Matijs van Zuijlen <matijs@matijs.net>
2018-03-09 10:12:37 +0100

    Fix example lengths

--- spec/dnote/notes_spec.rb
+++ spec/dnote/notes_spec.rb
@@ -1,46 +1,48 @@
 require 'spec_helper'
 
 describe(DNote::Notes) do
+  let(:line) { '# TODO: Do something or another!' }
+  let(:file) { 'foo.rb' }
+  let(:lineno) { 1 }
+
   describe('#labels') do
     it('returns the list of labels') do
       notes = described_class.new([], labels: ['TODO'])
       expect(notes.labels).to eq(['TODO'])
     end
   end
+
   describe('#files') do
     it('returns the files attribute') do
-      notes = described_class.new(['example1.rb'])
-      expect(notes.files).to eq(['example1.rb'])
+      notes = described_class.new([file])
+      expect(notes.files).to eq([file])
     end
   end
 
   describe('#files=') do
     it('changes the paths attribute') do
       notes = described_class.new([])
-      notes.files = ['example1.rb']
-      expect(notes.files).to eq(['example1.rb'])
+      notes.files = [file]
+      expect(notes.files).to eq([file])
     end
   end
+
   describe('#match_general') do
     it('works') do
       notes = described_class.new([])
-      line = '# TODO: Do something or another!'
-      lineno = 1
-      file = 'foo.rb'
       rec = notes.match_general(line, lineno, file)
       expect(rec.to_h).to eq('label' => 'TODO', 'file' => file, 'line' => lineno, 'text' => 'Do something or another!')
     end
   end
+
   describe('#match_special') do
     it('works') do
       notes = described_class.new([], labels: ['TODO'])
-      line = '# TODO: Do something or another!'
-      lineno = 1
-      file = 'foo.rb'
       rec = notes.match_special(line, lineno, file)
       expect(rec.to_h).to eq('label' => 'TODO', 'file' => file, 'line' => lineno, 'text' => 'Do something or another!')
     end
   end
+
   describe('#counts') { it { skip('pending') } }
   describe('#notes') { it { skip('pending') } }
   describe('#parse') { it { skip('pending') } }
